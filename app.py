538ec680c385a835c3da07c5afa7c0715b5e2a65
leandrocl2005 <leandrocl2005@yahoo.com.br>
2020-09-19 21:45:17 -0300

    loader

--- app.py
+++ app.py
@@ -5,6 +5,13 @@ from werkzeug.utils import secure_filename
 from flask import send_from_directory
 import cv2
 
+from PIL import Image
+from io import BytesIO
+import numpy as np
+import base64
+
+import time
+
 
 app = Flask(__name__)
 app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024
@@ -26,10 +33,25 @@ def upload(filename):
     return send_from_directory(app.config['UPLOAD_PATH'], filename)
 
 
-def convert2gray(filename):
+def convert2gray_from_filename(filename):
     image_from_upload = cv2.imread(filename)
     gray_image = cv2.cvtColor(image_from_upload, cv2.COLOR_BGR2GRAY)
-    cv2.imwrite(filename.split('.')[0] + "_gray.jpg", gray_image)
+    return gray_image
+
+def convert2gray_from_image(image):
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    time.sleep(5)
+    return gray_image
+
+def create_uri_image_from_numpy_image(image):
+    img = Image.fromarray(image.astype("uint8"))
+    rawBytes = BytesIO()
+    img.save(rawBytes, "JPEG")
+    rawBytes.seek(0)
+    img_base64 = base64.b64encode(rawBytes.getvalue()).decode('ascii')
+    mime = "image/jpeg"
+    uri = "data:%s;base64,%s"%(mime, img_base64)
+    return uri
 
 
 @app.route('/')
@@ -43,12 +65,20 @@ def index():
 def upload_files():
     uploaded_file = request.files['file']
     filename = secure_filename(uploaded_file.filename)
+
+    byte_image = uploaded_file.stream.read()
+    numpy_image = np.array(Image.open(BytesIO(byte_image)))
+
     if filename != '':
         file_ext = os.path.splitext(filename)[1]
         if file_ext not in app.config['UPLOAD_EXTENSIONS']:
             abort(400)
-        uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
-        convert2gray("uploads/" + filename)
+        # uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
+        
+        gray_image = convert2gray_from_image(numpy_image)
+        uri_image = create_uri_image_from_numpy_image(gray_image)
+
+        return render_template('index.html', image=uri_image)      
     return redirect(url_for('index'))
 
 
